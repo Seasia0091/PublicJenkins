035cc82b8a20ca6656a61ea3804c99d67a1d22d7
seasiainfotech <psingh1@seasiainfotech.com>
2019-02-11 17:05:24 +0530

    Initial Commit

---
+++ JenkinsExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JenkinsExample
+//
+//  Created by Paramvir Singh on 2/11/19.
+//  Copyright © 2019 Param. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
